d93b93ae13a3f24e2657a6364f9181376453e0e4
Andre Khmelnitsky <akhmelni@uci.edu>
2024-04-18 20:18:10 -0700

    basic changes to tokenizer, still a lot of work to be done tonight

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -11,11 +11,7 @@ enum Token {
     SEMICOLON, 
     PERIOD,
     COMPUTATION, 
-    VAR, 
-
-
-
-
+    VAR
 }
 
 #[derive(Debug, Default)]
@@ -30,6 +26,7 @@ fn build_tokenizer(input_str: &str) -> Tokenizer {
         position: 0,
     }
 }
+
 
 
 
@@ -55,6 +52,32 @@ impl Tokenizer {
         self.vec_string[self.position]
     }
 
+    fn build_identifier(&mut self) -> String{
+        let mut ret_str = String::new(); 
+        while self.peek_curr_char().unwrap_or('\0').is_alphanumeric(){
+            ret_str.push(self.peek_curr_char_unsafe());
+            self.next()
+        }
+
+        ret_str
+    }
+
+    fn build_number(&mut self) -> usize {
+        let mut ret_num: usize = 0; 
+        while self.peek_curr_char().unwrap_or('\0').is_alphanumeric(){
+            ret_num *= 10; 
+            // ret_num += ((self.peek_curr_char_unsafe()).to_digit(10)).unwrap();
+            ret_num += self.peek_curr_char_unsafe().to_digit(10).unwrap() as usize;
+
+        }
+        ret_num
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.peek_curr_char().unwrap_or('\0').is_whitespace(){
+            self.position += 1;
+        }
+    }
 
 
     
